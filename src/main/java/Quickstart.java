64766b2bb9b07dfe9774860abf527610f63839f5
hunglam95 <hunglamspk@gmail.com>
2017-03-05 15:07:30 +0700

    Update Upload

--- src/main/java/Quickstart.java
+++ src/main/java/Quickstart.java
@@ -1,14 +1,16 @@
+package main.java;
+
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.FileContent;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
-
 import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.*;
 import com.google.api.services.drive.Drive;
@@ -99,20 +101,32 @@ public class Quickstart {
         // Build a new authorized API client service.
         Drive service = getDriveService();
 
-        // Print the names and IDs for up to 10 files.
-        FileList result = service.files().list()
-             .setPageSize(10)
-             .setFields("nextPageToken, files(id, name)")
-             .execute();
-        List<File> files = result.getFiles();
-        if (files == null || files.size() == 0) {
-            System.out.println("No files found.");
-        } else {
-            System.out.println("Files:");
-            for (File file : files) {
-                System.out.printf("%s (%s)\n", file.getName(), file.getId());
-            }
-        }
+//        // Print the names and IDs for up to 10 files.
+//        FileList result = service.files().list()
+//             .setPageSize(10)
+//             .setFields("nextPageToken, files(id, name)")
+//             .execute();
+//        List<File> files = result.getFiles();
+//        if (files == null || files.size() == 0) {
+//            System.out.println("No files found.");
+//        } else {
+//            System.out.println("Files:");
+//            for (File file : files) {
+//                System.out.printf("%s (%s)\n", file.getName(), file.getId());
+//            }
+//        }
+    	File fileMetadata = new File();
+    	fileMetadata.setName("Example.txt");
+    	java.io.File filePath = new java.io.File("photo.jpg");
+    	FileContent mediaContent = new FileContent("image/jpeg", filePath);
+    	File file = service.files().create(fileMetadata, mediaContent)
+    	        .setFields("id")
+    	        .execute();
+    	System.out.println("File ID: " + file.getId());
     }
 
+	
+
+    
+    
 }
